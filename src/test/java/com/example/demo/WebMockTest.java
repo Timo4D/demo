510fd93c2650f32780bf693c8414f0cb81d0d974
Timo Gerstenhauer <timo.gerstenhauer@gmx.de>
2023-05-09 15:56:58 +0200

    More Tests

--- src/test/java/com/example/demo/WebMockTest.java
+++ src/test/java/com/example/demo/WebMockTest.java
@@ -1,28 +1,45 @@
 package com.example.demo;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-//Startet nur den WebLayer und nicht den ganzen Context
+import com.example.video.Video;
+import com.example.video.VideoController;
+import com.example.video.VideoService;
 
-@WebMvcTest
-public class WebLayerTest {
+@WebMvcTest(VideoController.class)
+public class WebMockTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private VideoService service;
+
+    private List<Video> testList= new ArrayList<Video>(Arrays.asList(
+        new Video("test", "test", "test", "test")
+    ));
+
     @Test
-    public void shouldReturnDefaultMessage() throws Exception {
+    public void greetingShouldReturnMessageFromService() throws Exception {
+        when(service.getVideoList()).thenReturn(testList);
         this.mockMvc.perform(get("/videos")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(
-                        "[{\"title\":\"Edge of Tomorrow\",\"ageRating\":\"16\",\"description\":\"In Zeitschleife gefangen bis Aliens vernichtet\",\"genre\":\"SciFi\"},{\"title\":\"Security\",\"ageRating\":\"18\",\"description\":\"Marine rettet MÃ¤dchen in Kaufhaus\",\"genre\":\"Action\"}]"));
+                    "[{\"title\":\"test\",\"ageRating\":\"test\",\"description\":\"test\",\"genre\":\"test\"}]"));
     }
 
 }
